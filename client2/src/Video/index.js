0c92aa2a9110a2876ead676673fa02c8ee6ab213
gdomorski <gregdomorski@gmail.com>
2016-01-26 12:29:37 -0500

    Made whiteboard work for React

--- client2/src/Video/index.js
+++ client2/src/Video/index.js
@@ -1,12 +1,19 @@
-
+// import _ from 'underscore';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+// import YTSearch from 'youtube-api-search';
+// import SearchBar from './components/search_bar';
+// import VideoList from './components/video_list';
+// import VideoDetail from './components/video_detail';
 import DrawableCanvas from 'react-drawable-canvas';
+// const API_KEY = 'AIzaSyACCRzAumvvEk2O2lCmS9CZTOVWfCJhaL0';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			// videos: [],
+			// selectedVideo: null,
 			brushColor: '#000000',
       lineWidth: 4,
       canvasStyle: {
@@ -14,7 +21,17 @@ class App extends Component {
       },
       clear: false
 	  };
+	  // this.videoSearch('hack reactor')
 	}
+	// videoSearch(term){
+	// 	console.log(this)
+	// 	YTSearch({key: API_KEY, term: term}, (videos) => {
+	// 		this.setState({
+	// 			videos: videos,
+	// 			selectedVideo: videos[0]
+	// 		 });
+	// 	});
+	// }
 
 	handleOnClickClear()  {
     this.setState({
@@ -53,7 +70,17 @@ class App extends Component {
 
 
 	render() {
+		// const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 500);
+
 		return (
+		 // <div>
+		 		// <SearchBar onSearchTermChange={videoSearch} />
+		 		// <VideoDetail video={this.state.selectedVideo} />
+		 		// <VideoList onVideoSelect={
+		 		// selectedVideo => this.setState({selectedVideo})}
+		 		// videos={this.state.videos} />
+
+
 		 		<div className='canvas-state' style={{height: '500px'}}>
 	        <p>Frolicking Ampersands</p>
         <div className='button-bar'>
@@ -65,6 +92,7 @@ class App extends Component {
         </div>
         <DrawableCanvas {...this.state}/>
         </div>
+		 // </div>
 		)
 	}
 };
