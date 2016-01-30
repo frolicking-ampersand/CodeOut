b3dcce37f8c24d84be24917fb130c0534d492c9c
Nikola Phipps <tesla.phipps@gmail.com>
2016-01-30 11:20:37 -0800

    (feat) added a mapping function to help construct the board list

--- client2/src/components/trueMenu.js
+++ client2/src/components/trueMenu.js
@@ -1,8 +1,17 @@
 import React from 'react';
 const ReactDOM = require('react-dom');
 const Menu = require('react-burger-menu').stack;
+import axios from 'axios';
 
 const NavBar = () => {
+  render() {
+    axios.get('api/boards')
+
+    var boardList = this.state.data.map(function(board) {
+      return <li>{board}</li>
+    });
+    return <ul>{boardList}<ul>
+  }
   return (
     <div>
     <Menu> 
