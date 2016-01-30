4fc33404782a911f0c3fe95d26fddc5520fb293c
Ian <ian.bulmer89@gmail.com>
2016-01-29 21:00:05 -0800

    changed database

--- server/db/db.js
+++ server/db/db.js
@@ -1,26 +1,17 @@
-// var Sequelize = require("sequelize");
-// var db = new Sequelize("board", "", "", {
-//   host: 'localhost',
-//   dialect: 'postgres'
-// });
-
-var pg = require('pg');
-
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
+var Sequelize = require("sequelize");
+var db = new Sequelize("board", "", "", {
+  host: 'localhost',
+  dialect: 'postgres'
 });
 
 
 // we define the models we need using js--we don't need a schema file!
-// var User = db.define('User', {
-//   google_id: Sequelize.STRING,
-//   google_token: Sequelize.STRING,
-//   facebook_id: Sequelize.STRING,
-//   facebook_token: Sequelize.STRING
-// });
-
-
+var User = db.define('User', {
+  google_id: Sequelize.STRING,
+  google_token: Sequelize.STRING,
+  facebook_id: Sequelize.STRING,
+  facebook_token: Sequelize.STRING
+});
 
 // puts a UserId column on each Message instance
 // also gives us the `.setUser` method, available inside the .success callback
@@ -30,26 +21,26 @@ pg.connect(process.env.DATABASE_URL, function(err, client) {
 // User.hasMany(Message);
 
 
-// User.sync()
-//   .then(function() {
-//     console.log('User Table has is definitely in our Postgres Database');
-//   });
+User.sync()
+  .then(function() {
+    console.log('User Table has is definitely in our Postgres Database');
+  });
 // creates these tables in MySQL if they don't already exist. Pass in {force: true}
 // to drop any existing user and message tables and make new ones.
 
 
-// var Board = db.define('Board', {
-//   thing: Sequelize.BLOB
-// });
+var Board = db.define('Board', {
+  thing: Sequelize.BLOB
+});
 
-// Board.sync()
-//   .then(function() {
-//     console.log('Board Table has is definitely in our Postgres Database');
-//   });
+Board.sync()
+  .then(function() {
+    console.log('Board Table has is definitely in our Postgres Database');
+  });
 
 
-// module.exports = {
-//   User: User,
-//   Board: Board
-// }
+module.exports = {
+  User: User,
+  Board: Board
+}
 
