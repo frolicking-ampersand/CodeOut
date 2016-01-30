561c3b71d72acc2bb31fdf63d6f8fd7af6c402be
Ian <ian.bulmer89@gmail.com>
2016-01-29 21:00:05 -0800

    changed database

--- server/db/db.js
+++ server/db/db.js
@@ -15,14 +15,12 @@ if(process.env.PORT){
 
 
 // we define the models we need using js--we don't need a schema file!
-// var User = db.define('User', {
-//   google_id: Sequelize.STRING,
-//   google_token: Sequelize.STRING,
-//   facebook_id: Sequelize.STRING,
-//   facebook_token: Sequelize.STRING
-// });
-
-
+var User = db.define('User', {
+  google_id: Sequelize.STRING,
+  google_token: Sequelize.STRING,
+  facebook_id: Sequelize.STRING,
+  facebook_token: Sequelize.STRING
+});
 
 // puts a UserId column on each Message instance
 // also gives us the `.setUser` method, available inside the .success callback
@@ -32,26 +30,26 @@ if(process.env.PORT){
 // User.hasMany(Message);
 
 
-// User.sync()
-//   .then(function() {
-//     console.log('User Table has is definitely in our Postgres Database');
-//   });
+User.sync()
+  .then(function() {
+    console.log('User Table has is definitely in our Postgres Database');
+  });
 // creates these tables in MySQL if they don't already exist. Pass in {force: true}
 // to drop any existing user and message tables and make new ones.
 
 
-// var Board = db.define('Board', {
-//   thing: Sequelize.BLOB
-// });
+var Board = db.define('Board', {
+  thing: Sequelize.BLOB
+});
 
-// Board.sync()
-//   .then(function() {
-//     console.log('Board Table has is definitely in our Postgres Database');
-//   });
+Board.sync()
+  .then(function() {
+    console.log('Board Table has is definitely in our Postgres Database');
+  });
 
 
-// module.exports = {
-//   User: User,
-//   Board: Board
-// }
+module.exports = {
+  User: User,
+  Board: Board
+}
 
