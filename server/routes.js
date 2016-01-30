9f05cbcec445b7bd6d43e3305aa926c27527b028
Ian <ian.bulmer89@gmail.com>
2016-01-29 21:05:55 -0800

    reverted server changes

--- server/routes.js
+++ server/routes.js
@@ -27,12 +27,13 @@ module.exports = function (app, express) {
   // A protected route
 
   // get one board
-  // app.get('/', function(request, response) {
-  //     var result = 'App is running'
-  //     response.send(result);
-  // }).listen(app.get('port'), function() {
-  //     console.log('App is running, server is listening on port ', app.get('port'));
-  // });
+
+  app.get('/', function(request, response) {
+      var result = 'App is running'
+      response.send(result);
+  }).listen(app.get('port'), function() {
+      console.log('App is running, server is listening on port ', app.get('port'));
+  });
 
   app.get('/api/boards', function (req, res) {
     Board.findOne()
