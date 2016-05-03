b5e26c8838d30f2e29f708095cd6d419632afc63
Ian <ian.bulmer89@gmail.com>
2016-05-03 10:49:38 -0700

    redirect goes to Heroku

--- server/routes.js
+++ server/routes.js
@@ -37,12 +37,12 @@ module.exports = function (app, express) {
   // get one board
 
 
-  app.get('/', function(request, response) {
-      var result = 'App is running'
-      response.send(result);
-  }).listen(app.get('port'), function() {
-      console.log('App is running, server is listening on port ', app.get('port'));
-  });
+  // app.get('/', function(request, response) {
+  //     var result = 'App is running'
+  //     response.send(result);
+  // }).listen(app.get('port'), function() {
+  //     console.log('App is running, server is listening on port ', app.get('port'));
+  // });
 
   app.get('/api/boards', function (req, res) {
     Board.findOne()
